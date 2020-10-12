753b5fb42e20bf28fc753f5a27e8043bed185749
LeHuynhGiangr <56813353+LeHuynhGiangr@users.noreply.github.com>
2020-10-13 00:16:43 +0700

    lehuynhgiang1219 update solution structure

---
+++ source_api/Data/CommonEntities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.CommonEntities
+{
+    abstract class BaseEntity<T>
+    {
+        public T Id { get; set; }
+    }
+}
---
+++ source_api/Data/Interfaces/IDateTracking.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Data.Interfaces
+{
+    interface IDateTracking
+    {
+        DateTime DateCreated { get; set; }
+        DateTime DateModified { get; set; }
+    }
+}
