d6e0d429b55fae2b18d2b4e16133b517f57130b1
LeHuynhGiangr <56813353+LeHuynhGiangr@users.noreply.github.com>
2020-11-01 18:44:39 +0700

    lehuynhgiang update data access layer

--- source_api/Data.EF/ProjectDbContext.cs
+++ source_api/Data.EF/ProjectDbContext.cs
@@ -8,20 +8,24 @@ using System.Linq;
 
 namespace Data.EF
 {
-    class ProjectDbContext : IdentityDbContext<AppUser, AppRole, System.Guid>
+    public  class ProjectDbContext : IdentityDbContext<AppUser, AppRole, System.Guid>
     {
         public ProjectDbContext(DbContextOptions dbContextOption) : base(dbContextOption)
         {
-
         }
 
+        //start declare entities 
         public DbSet<AppUser> AppUsers { get; set; }
         public DbSet<AppRole> AppRoles { get; set; }
+        //end declare entites
 
         //this method is called on event model creating
+        //take an instance of ModelBuilder as a parameter, this is called by framework when db-context is first created to build the model and its mappings in memory.
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            //configure using fluent API
+            //start configure using fluent API
+            //builder.ApplyConfiguration()
+            //end configure
 
             base.OnModelCreating(builder);
         }
---
+++ source_api/Data.EF/ProjectDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+/// <summary>
+/// tell tools how to create db-context by implement the IDesignTimeDbContextFactory<TContext>
+/// if a class implementing this interface is found in either the same project as the derived DbContext or in
+/// the application's startup project, the tools bypass the other ways of creating the Dbcontext and use the
+/// design-time factory instead.
+/// </summary>
+namespace Data.EF
+{
+    //declaring db-context at desing-time
+    public  class ProjectDbContextFactory : IDesignTimeDbContextFactory<ProjectDbContext>
+    {
+        public ProjectDbContext CreateDbContext(string[] args)
+        {
+            //start get info-config from json file, auto reload if file is changed
+            ConfigurationBuilder l_configuration = new ConfigurationBuilder();
+            l_configuration.SetBasePath(Directory.GetCurrentDirectory());
+            l_configuration.AddJsonFile(path: "./appsettings.json", optional: false, reloadOnChange: true);
+            var l_configurer = l_configuration.Build();
+            //end
+
+            var l_optionsBuilder = new DbContextOptionsBuilder<ProjectDbContext>();
+            l_optionsBuilder.UseSqlServer(connectionString: l_configurer["ConnectionStrings:db1"]);
+
+            //return ProjectDbContext within configured DbContext
+            return new ProjectDbContext(l_optionsBuilder.Options);
+        }
+    }
+}
