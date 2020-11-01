599c60db3ded40584228ea1703c672b8740cd638
Ngô Đặng Đồng Khởi <ngodangdongkhoi@gmail.com>
2020-11-02 01:49:09 +0700

    Revert "lehuynhgiang update data access layer (Data.EF)"
    This reverts commit bfd730bd52ba2bd22bf93a0bc0dddbaa19e80a64.

--- source_api/Data.EF/ProjectDbContext.cs
+++ source_api/Data.EF/ProjectDbContext.cs
@@ -1,24 +1,22 @@
 ﻿/*
  * Configuring database context
  */
-using Data.EF.Configurations;
 using Data.Entities;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
+using System.Linq;
 
 namespace Data.EF
 {
-    public  class ProjectDbContext : IdentityDbContext<User, Role, System.Guid>
+    public  class ProjectDbContext : IdentityDbContext<AppUser, AppRole, System.Guid>
     {
         public ProjectDbContext(DbContextOptions dbContextOption) : base(dbContextOption)
         {
         }
 
         //start declare entities 
-        public DbSet<User> AppUsers { get; set; }
-        public DbSet<Role> AppRoles { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppRole> AppRoles { get; set; }
         //end declare entites
 
         //this method is called on event model creating
@@ -26,15 +24,7 @@ namespace Data.EF
         protected override void OnModelCreating(ModelBuilder builder)
         {
             //start configure using fluent API
-            //config asp identity
-            builder.ApplyConfiguration(new UserConfiguration());
-            builder.ApplyConfiguration(new RoleConfiguration());
-            builder.Entity<IdentityUserClaim<Guid>>().ToTable("userclaims");
-            builder.Entity<IdentityUserToken<Guid>>().ToTable("usertokens");
-            builder.Entity<IdentityUserLogin<Guid>>().ToTable("userlogins");
-            builder.Entity<IdentityUserRole<Guid>>().ToTable("userroles");
-            builder.Entity<IdentityRoleClaim<Guid>>().ToTable("roleclaims");
-            //end config asp identity
+            //builder.ApplyConfiguration()
             //end configure
 
             base.OnModelCreating(builder);
---
+++ source_api/Data/Entities/AppRole.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * class for role entity
+ */
+using Microsoft.AspNetCore.Identity;
+
+namespace Data.Entities
+{
+    public class AppRole : IdentityRole<System.Guid>
+    {
+        //description role
+        public string Description { get; set; }
+    }
+}
