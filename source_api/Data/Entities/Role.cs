9beb596c660b7aa8a869e9a0f62e20ffde50300e
LeHuynhGiangr <56813353+LeHuynhGiangr@users.noreply.github.com>
2020-11-03 19:42:07 +0700

    lehuynhgiang reset wrong commit

---
+++ source_api/Data/Entities/Role.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * class for role entity
+ */
+using Data.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Data.Entities
+{
+    public class Role : IdentityRole<System.Guid>, IDateTracking
+    {
+        //description role
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
