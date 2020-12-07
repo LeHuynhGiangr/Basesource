b72d13dd33666e8b3442532884edc018bf40bdd6
Ngô Đặng Đồng Khởi <ngodangdongkhoi@gmail.com>
2020-12-07 20:46:22 +0700

    ngodangdongkhoi update code
    update header

--- source_clientside/src/app/layout/header/header.component.ts
+++ source_clientside/src/app/layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppUsers } from './../../login/shared/login.model';
 import { LoginService } from './../../login/shared/login.service';
+import { UriHandler } from 'src/app/_helpers/uri-handler';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
@@ -10,22 +11,18 @@ import { LoginService } from './../../login/shared/login.service';
 export class HeaderComponent implements OnInit {
     appUsers:Array<AppUsers>
     public Image: string
-    constructor(private router: Router ,private service: LoginService) {}
+    constructor(private router: Router ,private service: LoginService, public uriHandler:UriHandler) {}
 
     async ngOnInit() {
       var user = await this.service.getUser();
       this.Image = user["avatar"]
     }
-    getImageMime(base64: string): string
-    {
-      return 'jpg';
-    }
-    getImageSource(base64: string): string
-    {
-      return `data:image/${this.getImageMime(base64)};base64,${base64}`; 
-    }
     getPath(){
       return this.router.url;
+    }
+    onLogout() {
+      this.service.logout();
+      this.router.navigateByUrl('/login');
     }
 }
 
